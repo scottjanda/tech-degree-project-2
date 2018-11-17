545b9d7720924072cd7f07da34b7fa209ca8e54f
Scott Janda <scottjanda@gmail.com>
2018-11-16 19:07:02 -0500

    Update list_pagination_and_filtering-v1/js/script.js

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -2,49 +2,61 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
+/***
+   Add your global variables that store the DOM elements you will
+   need to reference and/or manipulate.
+
+   But be mindful of which variables should be global and which
+   should be locally scoped to one of the two main functions you're
+   going to create. A good general rule of thumb is if the variable
+   will only be used inside of a function, then it can be locally
    scoped to that function.
 ***/
 
+//Determine number of students in list
+let numberOfStudents = document.getElementsByClassName('student-item').length;
+
+//Retrive full list of Students
+let studentList = document.getElementsByClassName('student-item');
 
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
+/***
+   Create the `showPage` function to hide all of the items in the
    list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
+   Pro Tips:
+     - Keep in mind that with a list of 54 students, the last page
        will only display four.
      - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+     - Remember that a function `parameter` goes in the parens when
+       you initially define the function, and it acts as a variable
+       or a placeholder to represent the actual function `argument`
+       that will be passed into the parens later when you call or
+       "invoke" the function
 ***/
 
+const showPage = (list, page) => {
+  for (i=0; i < numberOfStudents; i++)
+    if (
+        i > 10) {
+          studentList[i].style.display = 'none';
+        } else {
+          studentList[i].style.display = 'block';
+        }
+};
 
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+/***
+   Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
 
-
+// const appendPageLinks = () => {}
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
